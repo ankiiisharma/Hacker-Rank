20eb443057a8bd4e1201173d70d05bc1e9d26d8f
Ankiiisharma <myselfankit.2003@gmail.com>
2023-06-09 21:35:40 +0530

    Functions CPP

---
+++ fn.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int max_of_four(int a, int b, int c, int d)
+{
+    if(a>b && a>c && a>d)
+    {
+        cout<<a<<endl;
+    }
+    else if(b>a && b>c && b>d)
+    {
+        cout<<b<<endl;
+    }
+    else if(c>a && c>b && c>d)
+    {
+        cout<<c<<endl;
+    }
+    else
+     {
+        cout<<d<<endl;
+    }
+}
+
+int main()
+{
+    int a, b, c, d;
+    cin>>a>>b>>c>>d;
+    int ans = max_of_four(a, b, c, d);
+    cout<<ans<<endl;
+    
+    return 0;
+}
